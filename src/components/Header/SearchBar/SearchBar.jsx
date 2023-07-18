f6a52df3568e6a80e8bdda1e7eda8bee07d2aca9
Eneas Junior <eneas.silva.jr@gmail.com>
2023-07-18 15:44:46 -0300

    Seted rook state for the search button to retrieve input

--- src/components/Header/SearchBar/SearchBar.jsx
+++ src/components/Header/SearchBar/SearchBar.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 
 import './SearchBar.css';
 
 function SearchBar() {
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <form className="search-bar">
       <input
         type="search"
+        value={searchValue}
         placeholder="Buscar produtos"
         className="search_input"
+        onChange={({ target }) => setSearchValue(target.value)}
         required
       />
-
       <button type="submit" className="search_button">
-        <BsSearch / >
+        <BsSearch />
       </button>
     </form>
   );
