50b2d76ba25d4565fdcb8428deaddd38685f596c
Eneas Junior <eneas.silva.jr@gmail.com>
2023-07-19 12:14:57 -0300

    Finished Header cat-status styles

--- src/components/Header/CartButton/CartButton.jsx
+++ src/components/Header/CartButton/CartButton.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import  {TiShoppingCart} from 'react-icons/ti';
 
+import './CartButton.css';
+
 const CartButton = () => {
   return (
     <button type="button" className="cart_button">
       <TiShoppingCart />
+      <span className="cart-status">1</span>
     </button>
   );
 };
