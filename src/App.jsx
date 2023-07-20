73727dafcbde6677dff7186b0d2a2ef4b23d91b5
Eneas Junior <eneas.silva.jr@gmail.com>
2023-07-20 19:06:12 -0300

    setting useState an useEffect hooks to get fetch response and show over console.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Products from './components/Header/Products/Products';
 
 function App() {
   return (
     <div>
       <Header />
+      <Products />
     </div>
   );
 }
